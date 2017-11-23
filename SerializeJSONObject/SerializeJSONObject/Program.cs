07bd54aee1ee4cfcceb6c6043879f918fc4a5ecf
henryhe9999 <henryhe9999@gmail.com>
2017-11-23 14:47:13 -0500

    Added Serialize JSON Object

---
+++ SerializeJSONObject/SerializeJSONObject/Program.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializeJSONObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account
+            {
+                Email = "james@example.com",
+                Active = true,
+                CreatedDate = new DateTime(2013, 1, 20, 0, 0, 0, DateTimeKind.Utc),
+                Roles = new List<string>
+                {
+                "User",
+                "Admin"
+                }
+            };
+            string json = JsonConvert.SerializeObject(account, Formatting.Indented);
+
+            Console.WriteLine(json);
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Account
+    {
+        public string Email { get; set; }
+        public bool Active { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public IList<string> Roles { get; set; }
+    }
+}
